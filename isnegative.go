e63a920d55b548c3d6cbc660339844b559b5eb4e
Winlynn00 <winlynn00@gmail.com>
2019-10-16 21:32:51 +0600

    negative

--- isnegative.go
+++ isnegative.go
@@ -1,11 +1,13 @@
-package piscine
+package main
 
 import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
-	if nb < 0 {
-		z01.PrintRune('T')
-	} else {
-		z01.PrintRune('F')
-	}
+        if nb < 0 {
+                z01.PrintRune('T')
+    } else {
+                z01.PrintRune('F')
+        }
+        z01.PrintRune('\n')
 }
+
