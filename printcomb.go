c1817c07a680ffc417c18bad5ae6222a15b64115
Winlynn00 <winlynn00@gmail.com>
2019-10-17 21:25:00 +0600

    02

--- printcomb.go
+++ printcomb.go
@@ -1,8 +1,8 @@
-package main
+package piscine
 
 import"github.com/01-edu/z01"
 
-func main() {
+func PrintComb() {
 	for a := '0'; a <= '9'; a++ {
 		for b := '0'; b <= '9'; b++ {
 			for c := '0'; c <= '9'; c++ {
