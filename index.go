d85cb3a0cbf1001a71e918e16051e05c92137569
winlynn00 <54890065+winlynn00@users.noreply.github.com>
2019-10-24 12:29:08 +0600

    Update index.go

--- index.go
+++ index.go
@@ -1,29 +1,16 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	if StrLen(s) > 0 && StrLen(toFind) > 0 {
-		index := -1
-		if StrLen(s) > StrLen(toFind) {
-			for i := 0; i < StrLen(s); i++ {
-				count := 0
-				startIndex := 0
-				for j := 0; j < StrLen(toFind); j++ {
-					if s[i] == toFind[j] && j == 0 {
-						startIndex = i
-					}
-					if s[i] == toFind[j] {
-						count++
-						i++
-					}
-				}
-				if count == StrLen(toFind) {
-					index = startIndex
-					break
-				}
-			}
-		}
-		return index
-	} else {
-		return 0
+	sToRune := []rune(s)
+	toFindToRune := []rune(toFind)
+	length := 0
+	for range toFindToRune {
+		length++
 	}
+	for index, str := range sToRune {
+		if length > 0 && str == toFindToRune[0] {
+			return index
+		}
+	}
+	return -1	
 }
