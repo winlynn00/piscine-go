dc8d8f7bcd03159919e85ef7d2cbee91d58d62d5
winlynn00 <54890065+winlynn00@users.noreply.github.com>
2019-10-24 11:46:34 +0600

    Update and rename r to index.go

---
+++ index.go
@@ -0,0 +1,16 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	sToRune := []rune(s)
+	toFindToRune := []rune(toFind)
+	length := 0
+	for range toFindToRune {
+		length++
+	}
+	for index, str := range sToRune {
+		if length > 0 && str == toFindToRune[0] {
+			return index
+		}
+	}
+	return -1	
+}
